1ec10f56465d1f0da49e27d3e4a4e12c80279444
User260901 <alexmofinadze0@gmail.com>
2026-02-21 17:30:31 +0400

    changed configuration of webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
+const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     entry: './src/index.js',
     mode: 'production',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+        publicPath: '/'
+    },
     plugins: [new HtmlWebpackPlugin({
-        template: "index.html"
+        template: "./index.html"
     })],
     module: {
         rules: [
